2fa6ff0170e922a4acbc0064f480c1c0f1f91a84
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-05-31 12:45:26 +0100

    basic-migrations

--- bought_events/apps.py
+++ bought_events/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BoughtEventConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bought_event'
+    name = 'bought_events'
--- liked_events/apps.py
+++ liked_events/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LikedEventConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'liked_event'
+    name = 'liked_events'
