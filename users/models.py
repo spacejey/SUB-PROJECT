fe6a1f7fd3004c2b0846ea1a07aae9b656768258
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-06-12 12:49:29 +0100

    change relationship

--- users/models.py
+++ users/models.py
@@ -4,5 +4,9 @@ from django.core.validators import URLValidator
 
 # Create your models here.
 class User(AbstractUser):
+    #liked_event(manyToMany)
+    
+    #bought_event(manyToMany)
+
     email = models.CharField(max_length=50)
     profile_image = models.URLField(validators=[URLValidator()])
