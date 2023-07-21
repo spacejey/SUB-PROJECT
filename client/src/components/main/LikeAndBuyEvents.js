bd45ea8943cffb262cbcee6d2c48f32b63dec3ce
Thom Clegg <tomclegg25@gmail.com>
2023-07-21 15:00:08 +0100

    oh my god its done

--- client/src/components/main/LikeAndBuyEvents.js
+++ client/src/components/main/LikeAndBuyEvents.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useCallback, useEffect, useState } from 'react'
 
 
-const LikeEvents = ({ getSavedEvents, savedEvents, getUser, loggedInUser, authenticated , liked, setLiked, bought , setBought, eventId, name, date, url, images }) => {
+const LikeEvents = ({ getSavedEvents, savedEvents, getUser, loggedInUser, authenticated, liked, setLiked, bought, setBought, eventId, name, date, url, images }) => {
   const [user, setUser] = useState([])
   const [userError, setUserError] = useState('')
 
@@ -20,14 +20,14 @@ const LikeEvents = ({ getSavedEvents, savedEvents, getUser, loggedInUser, authen
   // ! FIXED - added savedEvents state to check against, and changed the includes method to some method.  
 
   // ! PROBLEM 2 : user can like and buy event simultaneously -  DONE
-  
+
   // ! TO DO, change event model , eventId unique = True
   // ! REFACTOR - lots of duplicate stuff 
 
   const handleEvent = async (type) => {
 
     // function for updating user model with liked or bought data
-    const updateLikedOrBoughtData = async (field,array) => {
+    const updateLikedOrBoughtData = async (field, array) => {
 
       const userResponse = await authenticated.put(`/api/users/${loggedInUser()}/`, {
         [field]: [array],
@@ -35,89 +35,60 @@ const LikeEvents = ({ getSavedEvents, savedEvents, getUser, loggedInUser, authen
       console.log(`USER ${field.toUpperCase()} UPDATED =>`, userResponse)
 
     }
-    
 
-    // function for removing event from liked/bought array when it's already been liked/bought
-    const removeEvent = async(type) => {
+    // function to check if a given event is in either the liked or bought array
+    const checkIfEventInOtherArray = (array, eventId) => {
 
-      const event = savedEvents.find( event => event.eventId === eventId)
-      console.log(event)
-      try {
-        const response = await updateLikedOrBoughtData( type, event.id )
-        console.log(response)
-        getUser()
-        console.log('removed')
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    
-    // function to check if a given event is in either liked or bought 
-    const checkIfEventInOtherArray = (array,eventId) => {
       return array.includes(eventId)
     }
-    
-    try {
-      
-      if (type === 'like') { 
-        if ( checkIfEventInOtherArray( bought, eventId )) {
-          console.log('in bought')
-        // If the event has already been liked , send put request to remove event from liked array
-        } else if (liked.includes(eventId)){
-          removeEvent('liked')
-          getUser()
 
-          // else if event is already in events, just update
-          // else , post and update
+    // Meaty function for button functionality :0
 
-        } else if ( savedEvents.some( event => event.eventId === eventId)) {
-          const event = savedEvents.find( event => event.eventId === eventId)
-          await updateLikedOrBoughtData('liked', event.id)
-          console.log('updated but not posted')
+    const eventConditional = async (oppositeArray, string ) => {
+      // if the event is in the other array, don't add event to current array
+      const event = savedEvents.find(event => event.eventId === eventId)
+      if (checkIfEventInOtherArray(oppositeArray, eventId)) {
+        console.log('in other array')
+
+        // Else if the event has already been liked , send a put request to remove event from current array
+      } else if (liked.includes(eventId)) {
+
+        await updateLikedOrBoughtData(string, event.id)
+        getUser()
+
+        // Else if event is already on the database, update the current array
+      } else if (savedEvents.some(event => event.eventId === eventId)) {
+        try {
+          await updateLikedOrBoughtData(string, event.id)
           getUser()
-          
-        } else  {
-          console.log(savedEvents)
-          const response = await authenticated.post('/api/events/', eventData)
-          console.log(savedEvents.includes( event => event.eventId === eventId))
-          await updateLikedOrBoughtData('liked', response.data.id)
-          console.log('updated and posted liked')
-          getUser()
-          getSavedEvents()
+        } catch (error) {
+          console.log(error)
         }
 
-        // If user clicks buy
-
-      } else if (type === 'buy') {
-        if (checkIfEventInOtherArray( liked, eventId)) {
-          console.log('in liked')
-        
-        // If the event has already been bought , send put request to remove event from bought array
-        } else if (bought.includes(eventId)){
-          removeEvent('bought')
-          getUser()
-
-          // else if event is already in events, just update
-          // else , post and update
-
-        } else if ( savedEvents.some( event => event.eventId === eventId)) {
-          const event = savedEvents.find( event => event.eventId === eventId)
-          await updateLikedOrBoughtData('bought', event.id)
-          console.log('updated but not posted')
-          getUser()
-          
-        } else  {
-          console.log(savedEvents)
-          const response = await authenticated.post('/api/events/', eventData)
-          console.log(savedEvents.includes( event => event.eventId === eventId))
-          await updateLikedOrBoughtData('bought', response.data.id)
-          console.log('updated and posted bought')
-          getUser()
-          getSavedEvents()
-        }
+        // Else post the event to the database and update the current array
       } else {
-        console.log( 'fallback')
+        const response = await authenticated.post('/api/events/', eventData)
+        try {
+          await updateLikedOrBoughtData(string, response.data.id)
+          getUser()
+          getSavedEvents()
+        } catch (error) {
+          console.log(error)
+        }
       }
+    }
+
+    try {
+
+      if (type === 'like') {
+        eventConditional(bought, 'liked')
+        // If user clicks buy
+      } else if (type === 'buy') {
+        eventConditional( liked, 'bought')
+      } else {
+        console.log('fallback')
+      }
+
     } catch (err) {
       console.log(err)
     }
@@ -131,7 +102,7 @@ const LikeEvents = ({ getSavedEvents, savedEvents, getUser, loggedInUser, authen
   const handleBuy = () => {
     handleEvent('buy')
   }
-  
+
   return (
     <>
       <button onClick={handleLike}>Like Event</button>
